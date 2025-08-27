641abfaf24cda25ea953b0b8b35472e62ea85db1
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-08-27 14:21:18 -0700

    Add link

--- flowkit.go
+++ flowkit.go
@@ -770,7 +770,7 @@ func (f *Flowkit) DeployProject(ctx context.Context, update UpdateContract) ([]*
 	}
 
 	if len(sorted) == 0 {
-		f.logger.Info(fmt.Sprintf("\n%s No contracts configured for deployment on network '%s'.\n\nTo add deployments, use 'flow config add deployment'.\nIf you meant to deploy to a different network, use the --network flag (e.g., 'flow project deploy --network testnet').", output.WarningEmoji(), f.network.Name))
+		f.logger.Info(fmt.Sprintf("\n%s No contracts configured for deployment on network '%s'.\n\nTo add deployments, use 'flow config add deployment'.\nIf you meant to deploy to a different network, use the --network flag (e.g., 'flow project deploy --network testnet').\n\nFor more details, see: https://developers.flow.com/build/tools/flow-cli/deployment/deploy-project-contracts", output.WarningEmoji(), f.network.Name))
 	} else {
 		f.logger.Info(fmt.Sprintf("\n%s All contracts deployed successfully", output.SuccessEmoji()))
 	}
